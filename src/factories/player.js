b930136e8f9b32aa9797f9b3c4b561990b0d116f
utkarshchaudhari <utkarshchaudhari1996@gmail.com>
2023-01-11 22:46:31 -0500

    making computer try next adjacent slot after getting a hit

--- src/factories/player.js
+++ src/factories/player.js
@@ -1,13 +1,24 @@
 export default function player(name, board, isComputer) {
   const attacks = [];
+  const lastAttacks = [];
   const attack = (x, y) => {
     if (isComputer) {
       while (true) {
         x = Math.floor(Math.random() * 10);
         y = Math.floor(Math.random() * 10);
+        if (lastAttacks[lastAttacks.length - 1] === true) {
+          x = attacks[attacks.length - 1].x + 1;
+          y = attacks[attacks.length - 1].y;
+          if (x > 9 || attacks.find((obj) => obj.x == x && obj.y == y)) {
+            lastAttacks.push(false);
+            continue;
+          }
+        }
         if (attacks.find((obj) => obj.x == x && obj.y == y)) continue;
         attacks.push({ x: x, y: y });
-        break;
+        const hit = board.receiveAttack(x, y);
+        lastAttacks.push(hit);
+        return hit;
       }
     }
     return board.receiveAttack(x, y);
